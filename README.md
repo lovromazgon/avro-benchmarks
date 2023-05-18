6ce2b0a283829ce6f5a7ab8fe82386c35e19535b
Lovro Mažgon <lovro.mazgon@gmail.com>
2023-05-18 18:28:39 +0200

    update dependencies, add hamba map benchmarks

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
 ![Logo](http://svg.wiersma.co.za/github/project?lang=go&title=avro-benchmarks)
 
+This fork includes the following changes:
+
+* All libraries are updated to the latest version (on 18.05.2023)
+  * Consequently, the models for gogen-avro are regenerated
+* It adds benchmarks `BenchmarkHambaDecodeMap` and `BenchmarkHambaEncodeMap`,
+  which test the speed of encoding and decoding a generic map using hamba, as
+  opposed to using a Go struct as the existing benchmarks. This is relevant for
+  use cases where your implementation does not know the Go struct in advance.
+
 #### Libraries
 
-* [github.com/hamba/avro](https://github.com/hamba/avro)
-* [github.com/go-avro/avro](https://github.com/go-avro/avro)
+* [github.com/hamba/avro/v2](https://github.com/hamba/avro)
+* [github.com/go-avro/avro](https://github.com/go-avro/avro) (unmaintained)
 * [github.com/linkedin/goavro/v2](https://github.com/linkedin/goavro)
-* [github.com/actgardner/gogen-avro/v9](https://github.com/actgardner/gogen-avro)
+* [github.com/actgardner/gogen-avro/v10](https://github.com/actgardner/gogen-avro)
 
 #### Results
 
-Go: 1.17 Machine: Macbook Pro 2,6 GHz Intel Core i7 16 GB 2133 MHz LPDDR3
+Go: 1.20.4
 
 ```
-BenchmarkGoAvroDecode-8      	  333607	      3182 ns/op	     418 B/op	      27 allocs/op
-BenchmarkGoAvroEncode-8      	  303241	      3975 ns/op	     839 B/op	      63 allocs/op
-BenchmarkGoGenAvroDecode-8   	  467928	      2603 ns/op	     728 B/op	      45 allocs/op
-BenchmarkGoGenAvroEncode-8   	 1500506	       878.0 ns/op	     256 B/op	       3 allocs/op
-BenchmarkHambaDecode-8       	 1898246	       558.9 ns/op	      64 B/op	       4 allocs/op
-BenchmarkHambaEncode-8       	 3031268	       399.7 ns/op	     112 B/op	       1 allocs/op
-BenchmarkLinkedinDecode-8    	  578799	      2084 ns/op	    1688 B/op	      35 allocs/op
-BenchmarkLinkedinEncode-8    	 1506548	       704.0 ns/op	     248 B/op	       5 allocs/op
+goos: darwin
+goarch: amd64
+pkg: github.com/nrwiersma/avro-benchmarks
+cpu: Intel(R) Core(TM) i9-9880H CPU @ 2.30GHz
+BenchmarkGoAvroDecode-16       	  431052	      2341 ns/op	     418 B/op	      27 allocs/op
+BenchmarkGoAvroEncode-16       	  352249	      2966 ns/op	     973 B/op	      63 allocs/op
+BenchmarkGoGenAvroDecode-16    	  865196	      1169 ns/op	     320 B/op	      11 allocs/op
+BenchmarkGoGenAvroEncode-16    	 2053659	       586.9 ns/op	     240 B/op	       3 allocs/op
+BenchmarkHambaDecode-16        	 3026668	       388.5 ns/op	      64 B/op	       4 allocs/op
+BenchmarkHambaEncode-16        	 4245242	       273.2 ns/op	     112 B/op	       1 allocs/op
+BenchmarkHambaDecodeMap-16     	  533870	      2216 ns/op	    1440 B/op	      36 allocs/op
+BenchmarkHambaEncodeMap-16     	  591298	      1967 ns/op	     112 B/op	       1 allocs/op
+BenchmarkLinkedinDecode-16     	  801688	      1531 ns/op	    1688 B/op	      35 allocs/op
+BenchmarkLinkedinEncode-16     	 2291268	       519.0 ns/op	     248 B/op	       5 allocs/op
+PASS
+ok  	github.com/nrwiersma/avro-benchmarks	16.426s
 ```
--- hamba_test.go
+++ hamba_test.go
@@ -3,7 +3,7 @@ package avro_benchmarks
 import (
 	"testing"
 
-	"github.com/hamba/avro"
+	"github.com/hamba/avro/v2"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -76,6 +76,85 @@ func BenchmarkHambaEncode(b *testing.B) {
 			{ID: 2345, Name: "Bone Claws", Damage: 5, Energy: 1.15, Passive: false},
 			{ID: 2346, Name: "Regeneration", Damage: -2, Energy: 0.55, Passive: true},
 			{ID: 2347, Name: "Adamant skeleton", Damage: -10, Energy: 0, Passive: true},
+		},
+	}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = avro.Marshal(schema, &superhero)
+	}
+}
+
+func TestHambaDecodeMap(t *testing.T) {
+	schema := avro.MustParse(Schema)
+
+	superhero := map[string]interface{}{}
+	err := avro.Unmarshal(schema, Payload, &superhero)
+
+	want := map[string]interface{}{
+		"id":             234765,
+		"affiliation_id": 9867,
+		"name":           "Wolverine",
+		"life":           float32(85.25),
+		"energy":         float32(32.75),
+		"powers": []interface{}{
+			map[string]interface{}{"id": 2345, "name": "Bone Claws", "damage": float32(5), "energy": float32(1.15), "passive": false},
+			map[string]interface{}{"id": 2346, "name": "Regeneration", "damage": float32(-2), "energy": float32(0.55), "passive": true},
+			map[string]interface{}{"id": 2347, "name": "Adamant skeleton", "damage": float32(-10), "energy": float32(0), "passive": true},
+		},
+	}
+	assert.NoError(t, err)
+	assert.Equal(t, want, superhero)
+}
+
+func BenchmarkHambaDecodeMap(b *testing.B) {
+	schema := avro.MustParse(Schema)
+
+	superhero := map[string]interface{}{}
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = avro.Unmarshal(schema, Payload, &superhero)
+	}
+}
+
+func TestHambaEncodeMap(t *testing.T) {
+	schema := avro.MustParse(Schema)
+
+	superhero := map[string]interface{}{
+		"id":             int32(234765),
+		"affiliation_id": int32(9867),
+		"name":           "Wolverine",
+		"life":           float32(85.25),
+		"energy":         float32(32.75),
+		"powers": []interface{}{
+			map[string]interface{}{"id": int32(2345), "name": "Bone Claws", "damage": float32(5), "energy": float32(1.15), "passive": false},
+			map[string]interface{}{"id": int32(2346), "name": "Regeneration", "damage": float32(-2), "energy": float32(0.55), "passive": true},
+			map[string]interface{}{"id": int32(2347), "name": "Adamant skeleton", "damage": float32(-10), "energy": float32(0), "passive": true},
+		},
+	}
+
+	b, err := avro.Marshal(schema, &superhero)
+
+	assert.NoError(t, err)
+	assert.Equal(t, Payload, b)
+}
+
+func BenchmarkHambaEncodeMap(b *testing.B) {
+	schema := avro.MustParse(Schema)
+
+	superhero := map[string]interface{}{
+		"id":             int32(234765),
+		"affiliation_id": int32(9867),
+		"name":           "Wolverine",
+		"life":           float32(85.25),
+		"energy":         float32(32.75),
+		"powers": []interface{}{
+			map[string]interface{}{"id": int32(2345), "name": "Bone Claws", "damage": float32(5), "energy": float32(1.15), "passive": false},
+			map[string]interface{}{"id": int32(2346), "name": "Regeneration", "damage": float32(-2), "energy": float32(0.55), "passive": true},
+			map[string]interface{}{"id": int32(2347), "name": "Adamant skeleton", "damage": float32(-10), "energy": float32(0), "passive": true},
 		},
 	}
 
